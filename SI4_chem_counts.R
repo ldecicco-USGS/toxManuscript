391a1541bc0432b259959aa86efaf09b8518484e
Laura DeCicco <ldecicco@usgs.gov>
2018-04-20 11:18:26 -0500

    Chem counts

---
+++ SI4_chem_counts.R
@@ -0,0 +1,30 @@
+library(toxEval)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+source(file = "data_setup.R")
+
+hit_threshold <- 10^-3
+
+tableData <- chemicalSummary %>%
+  group_by(site, date, chnm) %>% 
+  summarize(sumEAR = sum(EAR)) %>%
+  group_by(site, chnm) %>%
+  summarize(meanEAR = max(sumEAR)) %>%
+  group_by(chnm) %>%
+  summarize(nSites = sum(meanEAR > hit_threshold)) %>%
+  data.frame() %>%
+  arrange(desc(nSites)) %>%
+  filter(nSites > 1)
+
+tableData$chnm <- factor(tableData$chnm, levels = tableData$chnm)
+
+chemPlot <- ggplot(tableData)+
+  geom_bar(aes(x=chnm, y=nSites),stat = "identity",fill = "steelblue") +
+  theme_bw() +
+  xlab("") +
+  ylab("Number of Sites\n with EARmax > 0.001") +
+  theme(axis.text.x = element_text( angle = 90,vjust=0.5,hjust = 1)) 
+
+chemPlot
