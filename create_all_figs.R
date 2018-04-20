b9d657321d86f67fa471862b2545897585efd33d
Laura DeCicco <ldecicco@usgs.gov>
2018-04-20 13:51:55 -0500

    Umm...

--- create_all_figs.R
+++ create_all_figs.R
@@ -1,22 +1,26 @@
 # Create all the figures....
 
-dir.create(file.path("plots"), showWarnings = FALSE)
+# Fig 1:
+
 
 # Fig 2:
 source("fig_2_site_counts.R")
-
+rm(list=ls())
 # Fig 3:
 source("fig_3_bio_boxplots.R")
-
+rm(list=ls())
+# SI 2:
+source("SI2_detection_levels.R")
+rm(list=ls())
 # SI 3 and 5: 
 source("SI3_heat.R")
-
+rm(list=ls())
 # SI 4:
 source("SI4_chem_counts.R")
-
+rm(list=ls())
 # SI 6:
 source("SI6_sub_families.R")
-
+rm(list=ls())
 # SI 7:
 source("SI7_class_stacks.R")
-
+rm(list=ls())
