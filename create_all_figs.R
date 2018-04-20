24bd9e346835d19474c09a5b6346ebf74d4a9615
Laura DeCicco <ldecicco@usgs.gov>
2018-04-20 11:45:47 -0500

    create all

---
+++ create_all_figs.R
@@ -0,0 +1,15 @@
+# Create all the figures....
+
+dir.create(file.path("plots"), showWarnings = FALSE)
+
+# Fig 2:
+source("fig_2_site_counts.R")
+
+# Fig 3:
+source("fig_3_bio_boxplots.R")
+
+# SI 3 and 5: 
+source("SI3_heat.R")
+
+# SI 4:
+source("SI4_chem_counts.R")
