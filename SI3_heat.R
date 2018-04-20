fe00e51ce1d1171c16e01dc93a52272c0df4a483
Laura DeCicco <ldecicco@usgs.gov>
2018-04-20 11:27:32 -0500

    Adding bio heat map

--- SI3_heat.R
+++ SI3_heat.R
@@ -9,3 +9,12 @@ source(file = "data_setup.R")
 heat <- plot_tox_heatmap(chemicalSummary,
                  tox_list$chem_site,
                  category = "Chemical")
+
+heat
+
+#SI 5
+bio_heat <- plot_tox_heatmap(chemicalSummary,
+                 tox_list$chem_site,
+                 category = "Biological",
+                 manual_remove = "Undefined")
+bio_heat
