a4c5719226a7f49cddc30977ef697c3f03c654b6
Laura DeCicco <ldecicco@usgs.gov>
2018-05-11 14:12:54 -0500

    y label

--- plot_tox_endpoints_manuscript.R
+++ plot_tox_endpoints_manuscript.R
@@ -140,8 +140,7 @@ plot_tox_endpoints_manuscript <- function(chemicalSummary,
   stackedPlot <- ggplot(graphData)+
     scale_y_log10(y_label,labels=toxEval:::fancyNumbers,breaks=pretty_logs_new) +
     theme_minimal() +
-    xlab("") +
-    ylab("ToxCast Endpoint Name") +
+    xlab("ToxCast Endpoint Name") +
     theme(axis.text.y = element_text(vjust = .25,hjust=1)) 
   
   if(!is.na(hit_threshold)){
