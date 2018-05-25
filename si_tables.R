1bd00fc480c5396241c77ab5520f13a4ecc5dcfb
Laura DeCicco <ldecicco@usgs.gov>
2018-05-25 15:12:11 -0500

    New table

--- si_tables.R
+++ si_tables.R
@@ -28,6 +28,8 @@ si_3_endpoints <- AOP %>%
                    endPoint=assay_component_endpoint_name, 
                    assay_source=assay_source_long_name),
             by="endPoint") 
+dir.create("tables", showWarnings = FALSE)
+write.csv(si_3_endpoints, file = "tables/SI3.csv", row.names = FALSE, na = "")
 
 # SI 5:
 #
@@ -63,6 +65,28 @@ nSite_table_wide <- nSite_table_wide[,c("Family", "subFamily","ID",
                           "endPoint", "nChems",
                           rev(levels(chemicalSummary$chnm))[rev(levels(chemicalSummary$chnm)) %in% names(nSite_table_wide)])]  
 
+dir.create("tables", showWarnings = FALSE)
+write.csv(nSite_table_wide, file = "tables/SI5.csv", row.names = FALSE, na = "")
+
 # New SI 6:
 #
-# 
+# Site | AOP ID | AOP name | EndPoint | Chemical | max EAR | mean EAR
+rm(chemicalSummary)
+source(file = "data_setup.R")
+
+si_6 <- chemicalSummary %>%
+  left_join(select(AOP_crosswalk, 
+                   endPoint=Component.Endpoint.Name, 
+                   ID=AOP..,
+                   AOP_name = AOP.Title), by="endPoint") %>%
+  left_join(select(tox_list$chem_site, site=SiteID, `Short Name`),by="site") %>%
+  group_by(ID, AOP_name, `Short Name`, date, chnm, endPoint) %>%
+  summarize(sumEAR = sum(EAR, na.rm = TRUE)) %>%
+  group_by(ID, AOP_name, `Short Name`, chnm, endPoint) %>%
+  summarise(meanEAR = mean(sumEAR, na.rm = TRUE),
+            maxEAR = max(sumEAR, na.rm = TRUE)) %>%
+  ungroup() %>%
+  filter(meanEAR > ear_thresh) %>%
+  rename(site=`Short Name`)
+
+write.csv(si_6, file = "tables/SI6.csv", row.names = FALSE, na = "")
