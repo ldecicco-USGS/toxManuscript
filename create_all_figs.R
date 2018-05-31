005c8c6ada5bc15e11531d34ce27950c024d17b7
Laura DeCicco <ldecicco@usgs.gov>
2018-05-31 15:57:11 -0500

    Should work better

--- create_all_figs.R
+++ create_all_figs.R
@@ -9,8 +9,8 @@ rm(list=ls())
 # Fig 3:
 source("fig_3_endpoint_boxplots.R")
 rm(list=ls())
-# Fig 3:
-source("fig_4_aop_priority.R")
+# Fig 4:
+source("fig_4_aop_priority.R", print.eval=TRUE)
 rm(list=ls())
 
 
