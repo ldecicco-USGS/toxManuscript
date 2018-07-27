9d194e0cb3ca4d40e04bec5e3203d65f046c92d2
Laura DeCicco <ldecicco@usgs.gov>
2018-07-27 00:49:32 -0500

    Cleaning up

--- SI7_boxplots_mixtures.R
+++ SI7_boxplots_mixtures.R
@@ -37,48 +37,143 @@ chemSummData_max <- chemicalSummary %>%
   group_by(ID, chnm, CAS, site, date) %>%
   summarize(maxEAR = max(EAR, na.rm = TRUE)) 
 
-filenm <- "plots/SI7_mixtureBoxplots.pdf"
-pdf(filenm)
+
 EAR_thresh <- 0.00001
 
 
-plot_dimensions <- list(c(0,0),c(3,4),c(2,3),c(2,3))
-margins <- c(2,0.5,4,0)
-outer_margins <- c(2,4,2,1)
+plot_dimensions <- list(c(0,0),c(3,4),c(2,3),c(1,3))
+margins <- c(4,0.5,1,0)
+outer_margins <- c(7,5,2,1)
 axis_text_cex <- 0.6
 title_text_cex <- 0.7
 
-for(i in 2:4) {
-  sub_Num_sites <- Num_sites_by_mixture %>%
-    filter(nChems == i,numSites>=4)
-  par(mfrow=plot_dimensions[[i]],mar=margins,oma=outer_margins)
-  for(j in 1:dim(sub_Num_sites)[1]){
-    CASnums <- strsplit(sub_Num_sites[j,"chemVector"],"; ")[[1]]
-    nMixSites <- sub_Num_sites[j,"numSites"]
-    chnms <- unique(as.data.frame(ToxCast_ACC)[which(ToxCast_ACC$casn %in% CASnums),"chnm"])
-    
-    subChemSummary <- chemSummData_max %>%
-      filter(maxEAR > EAR_thresh) %>%
-      group_by(site,date) %>%
-      filter(grepl(paste(CASnums,collapse="|"), CAS)) %>%
-      group_by(site,date,ID) %>%
-      summarize(EARsum = sum(maxEAR))
-    
-    yaxis_plot_nums <- plot_dimensions[[i]][1]*j-1
-    yaxt <- ifelse(j %in%  (0:4*plot_dimensions[[i]][2] +1),"s","n")
-    boxplot(subChemSummary$EARsum ~ as.character(subChemSummary$ID), 
-            log="y",main=paste(chnms),
-            las=2,
-            ylim=c(1e-5,10),
-            cex.axis=axis_text_cex,
-            cex.main=title_text_cex,
-            yaxt=yaxt)
-    mtext(paste(nMixSites,"Sites"),side=3,line=-1,cex=0.7)
-    mtext("AOP",side=1,outer=TRUE)
-    mtext("EAR sum by sample",side = 2,line=2.5,outer=TRUE)
-  }
+y_label <- toxEval:::fancyLabels("Biological", FALSE, TRUE, FALSE, TRUE)
+y_label$caption <- "i = chemicals in an AOP, j = samples, k = sites"
+###################################
+i <- 2
+filenm <- "plots/SI7_mixtureBoxplots_A.pdf"
+pdf(filenm)
+sub_Num_sites <- Num_sites_by_mixture %>%
+  filter(nChems == i,numSites>=4)
+par(mfrow=plot_dimensions[[i]],mar=margins,oma=outer_margins)
+for(j in 1:dim(sub_Num_sites)[1]){
+  CASnums <- strsplit(sub_Num_sites[j,"chemVector"],"; ")[[1]]
+  nMixSites <- sub_Num_sites[j,"numSites"]
+  chnms <- unique(as.data.frame(ToxCast_ACC)[which(ToxCast_ACC$casn %in% CASnums),"chnm"])
   
-  mtext(paste(i,"-Compound Mixtures"),outer=TRUE)
+  subChemSummary <- chemSummData_max %>%
+    filter(maxEAR > EAR_thresh) %>%
+    group_by(site,date) %>%
+    filter(grepl(paste(CASnums,collapse="|"), CAS)) %>%
+    group_by(site,date,ID) %>%
+    summarize(EARsum = sum(maxEAR))
+  
+  yaxis_plot_nums <- plot_dimensions[[i]][1]*j-1
+  yaxt <- ifelse(j %in%  (0:4*plot_dimensions[[i]][2] +1),"s","n")
+  boxplot(subChemSummary$EARsum ~ as.character(subChemSummary$ID), 
+          log="y",main=paste(chnms),
+          las=2,
+          ylim=c(1e-5,10),
+          cex.axis=axis_text_cex,
+          cex.main=title_text_cex,
+          yaxt=yaxt)
+  mtext(paste(nMixSites,"Sites"),side=3,line=-1,cex=0.7)
+  
+  
 }
+mtext("AOP ID",side=1,outer=TRUE, line = -1.5, cex = 0.65)
+mtext(bquote(.(y_label[["y_label"]])),
+      side = 2,line=3,outer=TRUE, cex = 0.75)
+mtext(side = 1, cex = 0.5,adj = 0,line = 2,
+      text = bquote(atop(bold("Figure SI-7"~.(LETTERS[i-1])~":") ~ "Boxplots of exposure activity ratios (" ~
+                           .(y_label[["y_label"]])  ~
+                           ") by adverse outcome pathway for" ~ .(i) ~ "-chemical mixtures present",
+                    "in samples that occurred at a minimum of 4 sites during monitoring of Great Lakes tributaries, 2010-2013." ~ italic(.(y_label$caption)))),outer=TRUE)
+
 dev.off()
-shell.exec(filenm)
+
+###################################
+i <- 3
+filenm <- "plots/SI7_mixtureBoxplots_B.pdf"
+pdf(filenm)
+sub_Num_sites <- Num_sites_by_mixture %>%
+  filter(nChems == i,numSites>=4)
+par(mfrow=plot_dimensions[[i]],mar=margins,oma=outer_margins)
+for(j in 1:dim(sub_Num_sites)[1]){
+  CASnums <- strsplit(sub_Num_sites[j,"chemVector"],"; ")[[1]]
+  nMixSites <- sub_Num_sites[j,"numSites"]
+  chnms <- unique(as.data.frame(ToxCast_ACC)[which(ToxCast_ACC$casn %in% CASnums),"chnm"])
+  
+  subChemSummary <- chemSummData_max %>%
+    filter(maxEAR > EAR_thresh) %>%
+    group_by(site,date) %>%
+    filter(grepl(paste(CASnums,collapse="|"), CAS)) %>%
+    group_by(site,date,ID) %>%
+    summarize(EARsum = sum(maxEAR))
+  
+  yaxis_plot_nums <- plot_dimensions[[i]][1]*j-1
+  yaxt <- ifelse(j %in%  (0:4*plot_dimensions[[i]][2] +1),"s","n")
+  boxplot(subChemSummary$EARsum ~ as.character(subChemSummary$ID), 
+          log="y",main=paste(chnms),
+          las=2,
+          ylim=c(1e-5,10),
+          cex.axis=axis_text_cex,
+          cex.main=title_text_cex,
+          yaxt=yaxt)
+  mtext(paste(nMixSites,"Sites"),side=3,line=-1,cex=0.7)
+  
+  
+}
+mtext("AOP ID",side=1,outer=TRUE, line = -1.5, cex = 0.65)
+mtext(bquote(.(y_label[["y_label"]])),
+      side = 2,line=3,outer=TRUE, cex = 0.75)
+mtext(side = 1, cex = 0.5,adj = 0,line = 2,
+      text = bquote(atop(bold("Figure SI-7"~.(LETTERS[i-1])~":") ~ "Boxplots of exposure activity ratios (" ~
+                           .(y_label[["y_label"]])  ~
+                           ") by adverse outcome pathway for" ~ .(i) ~ "-chemical mixtures present",
+                         "in samples that occurred at a minimum of 4 sites during monitoring of Great Lakes tributaries, 2010-2013." ~ italic(.(y_label$caption)))),outer=TRUE)
+
+dev.off()
+
+################################
+i <- 4
+filenm <- "plots/SI7_mixtureBoxplots_C.pdf"
+pdf(filenm, height = 4)
+sub_Num_sites <- Num_sites_by_mixture %>%
+  filter(nChems == i,numSites>=4)
+par(mfrow=plot_dimensions[[i]],mar=margins,oma=outer_margins)
+for(j in 1:dim(sub_Num_sites)[1]){
+  CASnums <- strsplit(sub_Num_sites[j,"chemVector"],"; ")[[1]]
+  nMixSites <- sub_Num_sites[j,"numSites"]
+  chnms <- unique(as.data.frame(ToxCast_ACC)[which(ToxCast_ACC$casn %in% CASnums),"chnm"])
+  
+  subChemSummary <- chemSummData_max %>%
+    filter(maxEAR > EAR_thresh) %>%
+    group_by(site,date) %>%
+    filter(grepl(paste(CASnums,collapse="|"), CAS)) %>%
+    group_by(site,date,ID) %>%
+    summarize(EARsum = sum(maxEAR))
+  
+  yaxis_plot_nums <- plot_dimensions[[i]][1]*j-1
+  yaxt <- ifelse(j %in%  (0:4*plot_dimensions[[i]][2] +1),"s","n")
+  boxplot(subChemSummary$EARsum ~ as.character(subChemSummary$ID), 
+          log="y",main=paste(chnms),
+          las=2,
+          ylim=c(1e-5,10),
+          cex.axis=axis_text_cex,
+          cex.main=title_text_cex,
+          yaxt=yaxt)
+  mtext(paste(nMixSites,"Sites"),side=3,line=-1,cex=0.7)
+  
+  
+}
+mtext("AOP ID",side=1,outer=TRUE, line = -1.5, cex = 0.65)
+mtext(bquote(.(y_label[["y_label"]])),
+      side = 2,line=3,outer=TRUE, cex = 0.75)
+mtext(side = 1, cex = 0.5,adj = 0,line = 2,
+      text = bquote(atop(bold("Figure SI-7"~.(LETTERS[i-1])~":") ~ "Boxplots of exposure activity ratios (" ~
+                           .(y_label[["y_label"]])  ~
+                           ") by adverse outcome pathway for" ~ .(i) ~ "-chemical mixtures present",
+                         "in samples that occurred at a minimum of 4 sites during monitoring of Great Lakes tributaries, 2010-2013." ~ italic(.(y_label$caption)))),outer=TRUE)
+
+dev.off()
