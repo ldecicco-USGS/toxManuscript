d0eacf0a465f51bc005f57af5a9b09a2ec0461ca
srcorsi-USGS <srcorsi@usgs.gov>
2018-09-25 08:12:14 -0500

    basic plots on endpoint availability

--- Endpoint_stats.R
+++ Endpoint_stats.R
@@ -2,7 +2,8 @@
 
 df <- read.csv("./Tables/SI4_2_nodash.csv",stringsAsFactors = FALSE)
 
-plot(df$Total~df$min_ACC,log="xy")
+plot(df$Total~df$min_ACC,log="x")
 plot(df$Total~df$Active,log="xy")
 plot(df$Total~df$Active,log="")
+plot(df$Total~df$Filtered,log="")
 
