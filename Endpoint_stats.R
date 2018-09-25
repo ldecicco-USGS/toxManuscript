0b9711105daf1a39a43da192d7258036626d0c20
srcorsi-USGS <srcorsi@usgs.gov>
2018-09-25 06:12:55 -0500

    Look at table SI4, endpoint stats.

---
+++ Endpoint_stats.R
@@ -0,0 +1,8 @@
+#Explore numbers of endpoints vs active and vs min_ACC
+
+df <- read.csv("./Tables/SI4_2_nodash.csv",stringsAsFactors = FALSE)
+
+plot(df$Total~df$min_ACC,log="xy")
+plot(df$Total~df$Active,log="xy")
+plot(df$Total~df$Active,log="")
+
